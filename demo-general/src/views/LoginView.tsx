6f4ec401feef70d636d7a085fc77f8a415cec8d2
yanglvfeng <yanglvfeng@croot.com>
2023-05-05 14:13:13 +0800

    add 添加登录页UI

--- demo-general/src/views/LoginView.tsx
+++ demo-general/src/views/LoginView.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import { Modal, Button, Form, Input } from 'antd';
+import 'antd/dist/antd.css';
 
 export const LoginView = () => {
-    return (
-        <div>LoginView</div>
-    );
-}
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
 
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return  <Modal title="登录" open centered footer={null} closable={false}>
+        <Form
+            name="basic"
+            labelCol={{ span: 4 }}
+            wrapperCol={{ span: 18 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="账号"
+                name="user"
+                rules={[{ required: true, message: '请输入账号！' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="密码"
+                name="password"
+                rules={[{ required: true, message: '请输入密码！' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 4, span: 18 }}>
+                <Button type="primary" htmlType="submit">
+                    提交
+                </Button>
+            </Form.Item>
+        </Form>
+    </Modal>
+}
