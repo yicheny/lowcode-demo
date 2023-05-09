018c568b5d8f5ecc0c0b49f9d6102a7c969c9a5d
yanglvfeng <yanglvfeng@croot.com>
2023-05-09 15:34:30 +0800

    update 项目、schema项、schema查询接口联调

--- demo-general/src/plugins/plugin-pages-manage/index.tsx
+++ demo-general/src/plugins/plugin-pages-manage/index.tsx
@@ -1,26 +1,25 @@
 import * as React from 'react'
 import {IPublicModelPluginContext} from "@alilc/lowcode-types";
-import {useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {project} from '@alilc/lowcode-engine';
 import {saveSchema} from "../../services/mockService";
-import {store} from "../../utils";
+import {LOGIN_KEY, store, tryExecute} from "../../utils";
 import {PROJECT_KEY, SCHEMA_ACTIVE_KEY} from "../../utils/Store";
 import {List,message,Select} from 'antd'
 import {MinusCircleOutlined, EditOutlined, PlusCircleOutlined} from '@ant-design/icons'
 import clsx from "clsx";
 import AddModal from "./opeartions/add";
 import classes from './index.module.scss'
-import {useOpen} from "../../hooks";
+import {useOpen, usePost} from "../../hooks";
 import DelModal from "./opeartions/del";
+import _ from 'lodash'
 
 const {Item} = List;
-const schema1 = require('./schema1.json');
-const schema2 = require('./schema2.json');
 
 type ItemInfo = {
     key: string,
     title: string,
-    data: any
+    // data: any
 }
 
 enum OPEN {
@@ -30,18 +29,17 @@ enum OPEN {
 }
 
 function PagesManage() {
-    const projectList = useProjectList()
-    const schemaList = useSchemaList();
     const [active, setActive] = useState(store.get(SCHEMA_ACTIVE_KEY))
     const [project,setProject] = useState(store.get(PROJECT_KEY))
     const {openInfo,setOpenInfo,checkOpenType,close} = useOpen()
+    const projectList = useProjectList()
+    const schemaList = useSchemaList(project);
+
+    useSwitchSchema(project, active)
 
     const selectSchema = (x:ItemInfo) => {
         if(store.get(SCHEMA_ACTIVE_KEY) === x.key) return
         setActive(x.key)
-        store.set(SCHEMA_ACTIVE_KEY, x.key)
-        store.set(PROJECT_KEY, project)
-        switchSchema(x)
     }
 
     const selectProject = (value:string) => {
@@ -50,13 +48,13 @@ function PagesManage() {
 
     return <>
         <div style={{marginLeft:16}}>
-            当前环境：<Select options={projectList} defaultValue={project} style={{ width: 120 }} onChange={selectProject}/>
+            当前环境：<Select placeholder={'请选择项目'} options={projectList} defaultValue={project} style={{ width: 120 }} onChange={selectProject}/>
+            <PlusCircleOutlined style={{fontSize:18,marginLeft:12}} onClick={() => setOpenInfo({type:OPEN.ADD})}/>
         </div>
         <List dataSource={schemaList} renderItem={(x: ItemInfo) => {
             return <Item key={x.key} className={clsx(classes.item, {[classes.active]: active === x.key})} onClick={()=>selectSchema(x)}>
                 <span>{x.title}</span>
                 <span>
-                <PlusCircleOutlined style={{fontSize:18}} onClick={() => setOpenInfo({type:OPEN.ADD})}/>
                 <EditOutlined style={{fontSize:18,marginLeft:12}} onClick={() => setOpenInfo({type:OPEN.EDIT})}/>
                 <MinusCircleOutlined style={{fontSize:18,marginLeft:12}} onClick={() => setOpenInfo({type:OPEN.DEL})}/>
             </span>
@@ -68,27 +66,56 @@ function PagesManage() {
     </>
 }
 
-function switchSchema(x: any) {
-    project.importSchema(x.data)
-    saveSchema()
-}
+function useSchemaList(itemID:number) {
+    const {data,doFetch} = usePost()
 
-function useSchemaList() {
+    useEffect(()=>{
+        tryExecute(async ()=>{
+            if(!itemID) return
+            await doFetch(`/query/schemaListQuery?itemID=${itemID}`)
+        })
+    },[itemID])
+
     return useMemo(() => {
-        return [
-            {title: "首页", key: "home", data: schema1},
-            {title: "测试页1", key: "test1", data: schema2},
-        ]
-    }, [])
+        return _.map(data,x=>({title:x.slName, key:x.slID}))
+    }, [data])
 }
 
 function useProjectList(){
-    return useMemo(()=>{
-        return [
-            { label:"otcd", value:'otcd'},
-            { label:"sbl", value:'sbl'},
-        ]
+    const {data,doFetch} = usePost();
+
+    useEffect(()=>{
+        tryExecute(async ()=>{
+            const loginInfo = store.get(LOGIN_KEY)
+            if(!loginInfo) return
+            await doFetch(`/query/projectListQuery?userID=${loginInfo.userID}`)
+        })
     },[])
+
+    return useMemo(()=>{
+        return _.map(data,x=>({ value: x.itemID, label:x.itemName}))
+    },[data])
+}
+
+function useSwitchSchema(itemID:number,slID:number){
+    const {doFetch} = usePost()
+
+    useEffect(()=>{
+        tryExecute(async ()=>{
+            if(!slID) return
+            const data = await doFetch(`/query/schemaInfoQuery?slID=${slID}`)
+            const schema = _.get(data,'siInfo')
+            console.log('schema',schema)
+            switchSchema(schema)
+
+            function switchSchema(schema: any) {
+                store.set(SCHEMA_ACTIVE_KEY, slID)
+                store.set(PROJECT_KEY, itemID)
+                project.importSchema(schema)
+                saveSchema()
+            }
+        })
+    },[itemID, slID])
 }
 
 const PagesManagePlugin = (ctx: IPublicModelPluginContext) => {
