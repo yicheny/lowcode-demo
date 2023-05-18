4920f182d24e6c8579e5989007a0f585e8652b34
yanglvfeng <yanglvfeng@croot.com>
2023-05-18 11:33:50 +0800

    add 附加api到window对象

--- demo-general/src/init.ts
+++ demo-general/src/init.ts
@@ -3,13 +3,28 @@ import {baseApi, bizApi} from "./utils";
 export async function appInit(){
     console.log('appInit start...')
 
+    await addWindowProps()
+
+    // await apiTest();
+
+    console.log('appInit end...')
+}
+
+//------------函数定义区----------------
+async function apiTest(){
     // ---测试baseApi---
-    const data = await baseApi.get('https://v1.hitokoto.cn',{ encode:'json' })
+    // const data = await baseApi.get('https://v1.hitokoto.cn',{ encode:'json' })
+    const data = await baseApi.get('https://v1.hitokoto.cn?encode=json')
     console.log('data', data)
 
     // ---测试bizApi---
     const bizData = await bizApi.post('/base-server/account/counterParty/queryList')
     console.log('bizData', bizData)
+}
 
-    console.log('appInit end...')
+async function addWindowProps(){
+    // @ts-ignore
+    window._baseApi = baseApi
+    // @ts-ignore
+    window._bizApi = bizApi
 }
