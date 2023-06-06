cc41cde07f3a1e47bd4c86c3d0ae709ff03bfe8c
yanglvfeng <yanglvfeng@croot.com>
2023-06-06 19:52:56 +0800

    add 添加并注册BizColumnSetter组件 设置联动功能

--- demo-general/src/setters/BizColumnSetter.tsx
+++ demo-general/src/setters/BizColumnSetter.tsx
@@ -1,19 +1,30 @@
 import * as React from "react";
-import {useCallback, useMemo} from "react";
+import {useCallback, useEffect, useMemo} from "react";
 import {Select} from '@alifd/next';
+import _ from 'lodash'
 
 interface BizColumnSetterProps {
-    value: string
+    value: string,
+    onChange: (value: string) => void,
 }
 
 export const BizColumnSetter: React.FC<BizColumnSetterProps> = (props) => {
-    const {value} = props;
+    const {value, onChange} = props;
     const option = useMockOption()
-    // console.log('BizColumnSetter:props',props)
 
     const handleChange = useCallback((value: string, actionType: string, item: Record<string, any>) => {
-        console.log('BizColumnSetter:onChange', value, actionType, item);
-    }, [])
+        // console.log('BizColumnSetter:onChange', value, actionType, item);
+        onChange(value)
+        setOption()
+        return null
+
+        function setOption(){
+            _.forEach(item.option,(value,key)=>{
+                // @ts-ignore
+                props.field.parent.setPropValue(key, value)
+            })
+        }
+    }, [onChange])
 
     return <Select
         showSearch
@@ -26,11 +37,11 @@ export const BizColumnSetter: React.FC<BizColumnSetterProps> = (props) => {
 function useMockOption() {
     return useMemo(() => {
         return [
-            {label: "公司", value: "公司", option: {align: "center"}},
-            {label: "单据金额", value: "单据金额", option: {}},
-            {label: "币种", value: "币种", option: {}},
-            {label: "完成进度", value: "完成进度", option: {}},
-            {label: "到账日期", value: "到账日期", option: {}},
+            {label: "公司", value: "公司", option: {align: "left"}},
+            {label: "单据金额", value: "单据金额", option: {align: "center"}},
+            {label: "币种", value: "币种", option: {align: "right"}},
+            {label: "完成进度", value: "完成进度", option: {align: "center"}},
+            {label: "到账日期", value: "到账日期", option: {align: "center"}},
         ]
     }, [])
 }
