46149b916144f87b40e848d9fa1de402c0a48bfd
yanglvfeng <yanglvfeng@croot.com>
2023-07-01 10:07:10 +0800

    add 添加模板页插件

--- demo-general/src/plugins/plugin-template-pages/TemplatePages.tsx
+++ demo-general/src/plugins/plugin-template-pages/TemplatePages.tsx
@@ -1,17 +1,24 @@
 import { List, Switch } from '@alifd/next';
 import {useCallback, useState} from "react";
+import {project} from "@alilc/lowcode-engine";
+
+const TestSchema = require('./mocks/test.json')
+const searchSchema = require('./mocks/searchTemplate.json')
 
 const MOCK_CONFIG = [
-    { text:"查询页", schema:null },
-    { text:"测试页1", schema:null },
-    { text:"测试页2", schema:null },
-    { text:"测试页3", schema:null },
+    { text:"查询页", schema:searchSchema },
+    { text:"测试页1", schema:TestSchema },
+    { text:"测试页2", schema:TestSchema },
+    { text:"测试页3", schema:TestSchema },
 ]
 
 export function TemplatePages(){
     const {onSelect,isSelect} = useSelect()
     return <List style={{padding:'0 16px'}} size={'small'} header={''}  dataSource={MOCK_CONFIG} renderItem={(o,i)=>{
-        return <List.Item extra={<Switch checked={isSelect(i)} onChange={(checked)=>onSelect(checked,i)}/>}>
+        return <List.Item extra={<Switch checked={isSelect(i)} onChange={(checked)=>{
+            onSelect(checked,i)
+            project.importSchema(o.schema)
+        }}/>}>
             {o.text}
         </List.Item>
     }}/>
@@ -21,7 +28,8 @@ function useSelect(){
     const [select,setSelect] = useState<number>(-1)
 
     const onSelect = useCallback((checked, id)=>{
-        setSelect(checked ? id : -1)
+        // setSelect(checked ? id : -1)
+        setSelect(id) //目前设置不允许取消
     },[])
 
     const isSelect = useCallback((id)=>{
